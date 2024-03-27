6c3416549a97b4bd0c5a48a5995575b3b6033694
hvandecar <122235170+hvandecar@users.noreply.github.com>
2024-03-27 11:41:30 -0400

    Add files via upload

---
+++ project8/js/workflow1.js
@@ -0,0 +1,313 @@
+class util {
+
+  static findTarget(evt, targetNode, container) {
+    let currentNode = evt.target;
+    while (currentNode && currentNode !== container) {  
+      if (currentNode.nodeName.toLowerCase() === targetNode.toLowerCase()) { return currentNode; }
+      else { currentNode = currentNode.parentNode; }
+    }
+    return false;
+  }
+}
+
+class dataTable {
+
+  constructor(theTableRef) {
+
+    this.theTable = document.querySelector(theTableRef);
+    this.theButtons = document.getElementById('actionForm');
+    this.rowHolder = document.querySelector('#projectsTbl tbody');
+    this.tableHeaders = this.theTable.querySelectorAll('thead th');
+    this.rows = this.rowHolder.querySelectorAll('tr');
+
+    //priority boolean
+    this.priority = false;
+    //highlight boolean
+    this.highlight = false;
+    //type boolean
+    this.type = false;
+    //boolean for if list is a-z or z-a
+    this.UptoDn = true;
+    //main order sequence
+    this.newValues = [];
+  }
+
+  init(){
+    const btn1 = document.createElement('input');
+    const btn2 = document.createElement('input');
+    const btn3 = document.createElement('input');
+    btn1.type = 'button';
+    btn1.name = 'actionButton';
+    btn1.value = 'Hide Low Priority';
+    btn2.type = 'button';
+    btn2.name = 'actionButton';
+    btn2.value = 'Highlight Current Row';
+    btn3.type = 'button';
+    btn3.name = 'actionButton';
+    btn3.value = 'Hide Type Column';
+    this.theButtons.appendChild(btn1);
+    this.theButtons.appendChild(btn2);
+    this.theButtons.appendChild(btn3);
+
+    //link up the headers as buttons
+    for (let j = 0; j < this.tableHeaders.length; j++) {
+        this.tableHeaders[j].innerHTML = "<a href='#'>" + this.tableHeaders[j].innerText + "</a>";
+    }
+    
+    //function that regenerates the table and all it's previous settings
+    this.afterTblLoad();
+    //function to generate the recent changes
+    this.updateRecentChanges();
+
+  }
+
+  async afterTblLoad() {
+
+    //function to generate the table
+    await this.updateTableValues();
+    
+    //reload the loaded columns
+    //get the local storage column number, turn into number not string
+    this.newValues = parseFloat(localStorage.getItem('mainList'));
+    //get the local storage column direction, turn into boolean not string
+    this.UptoDn = localStorage.getItem('direction') === 'false' ? false : true;
+
+    if (isNaN(this.newValues)) { return; }
+
+    //change the button depending on the column direction
+    if (this.UptoDn === true) {
+      this.tableHeaders[this.newValues].classList.add('downArrow');
+      this.sortRows(this.newValues);
+    }
+    else {
+      this.tableHeaders[this.newValues].classList.add('upArrow');
+      this.sortRows(this.newValues);
+    }
+
+    //reference to turning which buttons on and off
+    this.priority = localStorage.getItem('priority');
+    if (this.priority === 'true') {
+      this.theTable.classList.add('hidePriority');
+      btn1.value = 'Show Low Priority';
+    }
+    this.highlight = localStorage.getItem('highlight');
+    if (this.highlight === 'true') {
+      this.theTable.classList.add('allowHighlight');
+      btn2.value = 'Remove Row Highlight';
+    }
+    this.type = localStorage.getItem('type');
+    if (this.type === 'true') {
+      this.theTable.classList.add('hideColumn');
+      btn3.value = 'Show Type Column';
+    }
+  }
+
+  async updateTableValues() {
+    try {
+      let response = await fetch('json/issues-data.json', { method: 'GET', cache: 'no-cache' });
+      let data = await response.json();
+      let totalNewChanges = data.issues.length;
+      //base string
+      let str = '';
+      //add in each lists item to the string
+      
+      for (let x = 0; x < totalNewChanges; x++) {
+        str += '<tr data-priority="' + data.issues[x].priority.toLowerCase()
+        str += '"><th scope="row">' + data.issues[x].id 
+        str += '</th><td>' + data.issues[x].type 
+        str += '</td><td><a href="' + data.issues[x].project + '-' + data.issues[x].id  + '.html">' + data.issues[x].desc + '</a></td><td>'
+        str += data.issues[x].priority + '</td><td>' 
+        str += data.issues[x].assigned + '</td></tr>';
+      }
+      //add it into html
+      this.rowHolder.insertAdjacentHTML('afterbegin', str);
+      this.rows = this.rowHolder.querySelectorAll('tr');
+    }
+    catch {
+      console.log(error);
+    }
+    
+  }
+
+  async updateRecentChanges() {
+    try {
+      let response = await fetch('json/recent-changes.json', { method: 'GET', cache: 'no-cache' });
+      let data = await response.json();
+      let totalNewChanges = data.changes.length;
+      //base string
+      let str = '<ul>';
+
+      //add in each lists item to the string
+      for (let x = 0; x < totalNewChanges; x++) {
+        str += '<li><a href="' + data.changes[x].id.toLowerCase() 
+        str += '.html" title="' + data.changes[x].txt + '">' 
+        str += data.changes[x].id + '</a> (' 
+        str += data.changes[x].status + ')</li>';
+      }
+      str += '</ul>';
+      //add it into html
+      document.getElementById('recentChanges').insertAdjacentHTML('beforeend', str);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  setDisplay(evt) {
+
+    const btn = util.findTarget(evt, 'input', evt.currentTarget);
+
+    if (!btn) { return; }
+  
+    // use button value to determine what to do
+    switch(btn.value) {
+    
+      case 'Hide Low Priority': 
+      
+        this.theTable.classList.add('hidePriority');
+        btn.value = 'Show Low Priority';
+        this.priority = true;
+        break;
+      
+      case 'Show Low Priority': 
+      
+        this.theTable.classList.remove('hidePriority');
+        btn.value = 'Hide Low Priority';
+        this.priority = false;
+        break;
+      
+      case 'Highlight Current Row': 
+      
+        this.theTable.classList.add('allowHighlight');
+        btn.value = 'Remove Row Highlight';
+        this.highlight = true;
+        break;
+    
+      case 'Remove Row Highlight': 
+      
+        this.theTable.classList.remove('allowHighlight');
+        btn.value = 'Highlight Current Row';
+        this.highlight = false;
+        break;
+      
+      case 'Hide Type Column': 
+      
+        this.theTable.classList.add('hideColumn');
+        btn.value = 'Show Type Column';
+        this.type = true;
+        break;
+      
+      case 'Show Type Column':
+
+        this.theTable.classList.remove('hideColumn');
+        btn.value = 'Hide Type Column';
+        this.type = false;
+        break;     
+      
+    }
+    // creates local storage for the buttons
+    localStorage.setItem('priority', this.priority);
+    localStorage.setItem('highlight', this.highlight);
+    localStorage.setItem('type', this.type);
+  }
+  
+  rowHighlight(evt) {
+
+    const row = util.findTarget(evt, 'tr', evt.currentTarget);
+    if (!row) { return; }
+    row.id = (row.id) ? '' : 'currentRow';
+  
+  }
+
+  sortingDirection(evt) {
+    evt.preventDefault();
+    const clickHeader = util.findTarget(evt, 'th', evt.currentTarget);
+    
+    if (!clickHeader) { return; }
+
+    //flip the a-z order and arrow when it has up arrow or down arrow
+    if (clickHeader.classList.contains('upArrow') || clickHeader.classList.contains('downArrow')) {
+      clickHeader.classList.toggle('upArrow');
+      clickHeader.classList.toggle('downArrow');
+      this.UptoDn = !this.UptoDn;
+    }
+    else {
+      for (let j = 0, allHdrs=this.tableHeaders.length; j < allHdrs; j++) {
+        this.tableHeaders[j].removeAttribute('class');
+        this.UptoDn = false;
+      }
+      clickHeader.classList.add('upArrow');
+    }
+    this.sortRows(this.siblingIndex(clickHeader));
+    
+  }
+
+  siblingIndex(node) {
+    let count = 0;
+    while (node = node.previousElementSibling) {
+        count++;
+    }
+    return count;
+    
+  }
+
+  sortRows(columnIndex) {
+    //node list to sort with the innertext inside
+    const values = [];
+    //node list needs to refresh every click because you need to clear the node list everyclick
+    this.newValues = [];
+
+    //this gets the text from each innerText, and checks if it is the first column and does that math
+    for (let index = 0; index < this.rows.length; index++) {
+      const node = this.rows[index].querySelector("td:nth-child(" + (columnIndex + 1) + ")") || this.rows[index].querySelector("th:nth-child(" + (columnIndex + 1) + ")");
+      const val = node.innerText;
+
+      values.push(val + '.' + [index]);
+      if (columnIndex > 0) {
+        values.sort();
+      }
+      else {
+        values.sort(function (num1,num2) {  
+          return num1 - num2;
+        });          
+      }
+    }
+
+    //has this list already been sorted? reverse it
+    if (this.UptoDn === false) {
+      values.reverse();
+    }
+
+    //take the list apart to find out what order we have to put the rows in
+    for (let order of values) {
+      const newVal = order.split('.').pop();
+      this.newValues.push(newVal);
+    }
+
+    //delete everyhting in the table and add it all back in
+    this.rowHolder.innerHTML = '';
+    for (let i=0, allDataRows=values.length; i<allDataRows; i++) {
+      this.rowHolder.appendChild(this.rows[this.newValues[i]])
+    }
+
+    //local storage for the order and if it is going up or down
+    localStorage.setItem('mainList', columnIndex);
+    localStorage.setItem('direction', this.UptoDn);
+  }
+
+}
+
+const openItems = new dataTable('#projectsTbl');
+openItems.init();
+
+const controlsHolder = document.querySelector('#actionForm');
+const rowHolder = document.querySelector('#projectsTbl tbody');
+const colHeaders = document.querySelector('#projectsTbl thead');
+
+//assign control button event listener
+controlsHolder.addEventListener('click', (evt) => openItems.setDisplay(evt), false); 
+//assign data row event listener
+rowHolder.addEventListener('mouseover', (evt) => openItems.rowHighlight(evt), false);
+rowHolder.addEventListener('mouseout', (evt) => openItems.rowHighlight(evt), false);
+//assign table header listener
+colHeaders.addEventListener('click', (evt) => openItems.sortingDirection(evt), false);
